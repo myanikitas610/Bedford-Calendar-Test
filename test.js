e4de6a5648e0cb2a3d2b9254859e9733d0303589
Mya Nikitas <mnikitas@fordham.edu>
2024-03-05 00:05:06 -0500

    test

--- test.js
+++
@@ -1,8 +0,0 @@
-const csvFilePath='/Users/myanikitas/Desktop/Bedford Calendar Test/test.csv'
-const csv=require('csvtojson')
-csv()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-    console.log(jsonObj);
-
-})
